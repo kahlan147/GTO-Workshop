f15ce75a079f1252c46ea373351090f4f7304bdc
Niels Verheijen <kahlan147@gmail.com>
2018-03-08 13:08:58 +0100

    Setup Week 1

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resource : MonoBehaviour {
+
+    public delegate void ChangedValues();
+    public static event ChangedValues ResourceChanged;
+
+    public int StartingAmount;
+    [HideInInspector]
+    public int CurrentAmount;
+
+
+	// Use this for initialization
+	void Start () {
+        this.CurrentAmount = StartingAmount;
+        ResourceChanged();
+	}
+
+    public void AddWood()
+    {
+        CurrentAmount += 1;
+        ResourceChanged();
+    }
+
+    public void RemoveWood()
+    {
+        CurrentAmount -= 1;
+        ResourceChanged();
+    }
+
+
+
+}
---
+++ Assets/Scripts/Wood.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Wood : MonoBehaviour {
+
+    public Text woodText;
+    public Resource resource;
+    
+    private void OnEnable()
+    {
+        Resource.ResourceChanged += ChangedResources;
+    }
+
+    private void ChangedResources()
+    {
+        woodText.text = "Wood: " + resource.CurrentAmount;
+    }
+}
